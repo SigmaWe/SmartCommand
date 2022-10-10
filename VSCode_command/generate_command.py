27cace4275bfb2278cdeb1757709aa1d06325aec
Superhzf <superhzf@hotmail.com>
2022-10-10 18:50:45 -0500

    type: feat
    Finish the scrip to clean the command

--- VSCode_command/generate_command.py
+++ VSCode_command/generate_command.py
@@ -1,10 +1,26 @@
 import re
+import os
 
 with open('raw_commands.txt') as f:
     lines = f.read()
 f.close()
 
-regex = "(\"command\": \")([\S\s]*)(\",\n)"
-compiled_regex = re.compile(regex,re.MULTILINE)
-commands = re.findall(compiled_regex, lines)
-print(commands[0])
+# regex1 extracts those commands that have a key with it
+regex1 = '(?<="command": ")([a-zA-Z0-9\.]*)'
+# regex2 extracts those commands that have no key with it
+regex2 = '(?<=\/\/ - )([a-zA-Z0-9\.]*)'
+commands_1st_half = re.findall(regex1, lines)
+commands_2nd_half = re.findall(regex2, lines)
+
+clean_command_file_name = "cleaned_commands.txt"
+try:
+    os.remove(clean_command_file_name)
+except OSError:
+    pass
+
+with open(clean_command_file_name,'a') as f:
+    for this_command in commands_1st_half:
+        f.write(f"{this_command}\n")
+    for this_command in commands_2nd_half:
+        f.write(f"{this_command}\n")
+f.close()
