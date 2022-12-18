2ad67ec1892343ba13fe37869601196b2be76259
jerryliu20d <33166299+jerryliu20d@users.noreply.github.com>
2022-12-18 10:07:03 -0600

    Add files via upload
    The minimal required files for local build.

---
+++ Plugin/smart-command/src/extension.ts
@@ -0,0 +1,26 @@
+/*
+// The module 'vscode' contains the VS Code extensibility API
+*/
+
+import { window, commands, ExtensionContext } from 'vscode';
+import { SingleStepSearch} from './basicInput';
+
+export function activate(context: ExtensionContext) {
+	context.subscriptions.push(commands.registerCommand('smart-command.NLPSearch', async () => {
+		/*
+		const options: { [key: string]: (context: ExtensionContext) => Promise<void> } = {
+			SingleStepSearch,
+		};
+		const quickPick = window.createQuickPick();
+		quickPick.items = Object.keys(options).map(label => ({ label }));
+		quickPick.onDidChangeSelection(selection => {
+			if (selection[0]) {
+				options[selection[0].label](context)
+					.catch(console.error);
+			}
+		});
+		quickPick.onDidHide(() => quickPick.dispose());
+		quickPick.show();*/
+		SingleStepSearch();
+	}));
+}
