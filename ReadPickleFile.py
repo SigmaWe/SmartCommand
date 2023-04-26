efcb2b9137b564be0a170918fef879c45ee8d320
Abdullah <98681495+Aljbab14@users.noreply.github.com>
2023-04-26 00:28:26 -0500

    Update ReadPickleFile.py

--- ReadPickleFile.py
+++ ReadPickleFile.py
@@ -1,7 +1,10 @@
 import pickle
 
 # Replace 'file_path.pickle' with the path to your pickle file
-pickle_file_path = 'PickleFile'
+
+pickle_file_path = 'CombinedPickleCommands.pkl'
+# pickle_file_path = 'PickleBuiltinCommands.pkl'
+# pickle_file_path = 'PicklePluginCommands.pkl'
 
 def read_pickle_file(file_path):
     with open(file_path, 'rb') as file:
