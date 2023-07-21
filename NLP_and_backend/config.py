b06b0d5f473b419a942ef16a08f795f01fb53fb5
parthiv2048 <parthiv2048@gmail.com>
2023-07-21 14:17:34 -0500

    Created pickle file for command title embeddings

--- NLP_and_backend/config.py
+++ NLP_and_backend/config.py
@@ -1,12 +1,13 @@
 data_root = "./"
 method = "sbert"
-#    model = 'all-MiniLM-L6-v2'
+model = 'all-MiniLM-L6-v2'
 # model = './pre_trained_models/multi-qa-mpnet-base-dot-v1'
-model = './pre_trained_models/all-MiniLM-L6-v2'
+# model = './pre_trained_models/all-MiniLM-L6-v2'
 
 # If you change the model above, be sure to change accordingly in run_all.sh as well
 
 import os
 model_name_base=os.path.basename(model)
-command_dict_list_json = os.path.join(data_root, "commands_processed.json")
+command_dict_list_json = os.path.join(data_root, "AllCommands.json")
 command_embedding_pickle = os.path.join(data_root, f"command_embeddings_{method}_{model_name_base}.pickle")
+command_embedding_pickle_title = os.path.join(data_root, f"command_embeddings_titles_{method}_{model_name_base}.pickle")
